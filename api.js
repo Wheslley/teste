daa40a0bf0bc8e9ceb8aa38cd908a6d70ea9e878
Wheslley <wheslleyny@gmail.com>
2018-04-04 21:23:42 -0300

    teste5

--- api.js
+++ api.js
@@ -4,9 +4,13 @@ module.exports = app => {
 	
     const MongoClient = require('mongodb').MongoClient;
     const ObjectID = require('mongodb').ObjectID;
-    const url = "mongodb://womanuser:womanuserapp@ds233769.mlab.com:33769/";
+    const url = 'mongodb://wheslley:senha123@ds231589.mlab.com:31589/womandb';
     const respostaSucesso = "{status:SUCESSO}";
     const respostaErro = "{status:ERRO}";
+	
+	app.get('/api/teste', (req, res) => {
+		res.json('{status:SUCESSO}');
+	});
 	
 	app.get('/api/select', (req, res) => {
 		console.log("inicio");
@@ -29,5 +33,28 @@ module.exports = app => {
             });
 		});
 	});
+	
+    app.post('/api/auditoria', (req, res) => {
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+            var dbo = db.db("womandb");
+			
+			dbo.collection("item").insertOne('{"nome":"celular"}', function (err, res) {
+				console.log("inico insert");
+				if (err) {
+					res.json(respostaErro);
+					throw err;
+				}
+				console.log("fim insert");
+			});
+			console.log("Inseriu");
+            dbo.collection("item").find({}).toArray(function (err, result) {
+                if (err) throw err;
+                if (!result) res.json(respostaErro);
+                else res.json(result);
+                db.close();
+            });
+        });
+    });
 
 }
